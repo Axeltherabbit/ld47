1c65389177cfb957c196de464ba0248b15012b4f
axeltherabbit <backb0x@hotmail.com>
2020-10-02 15:50:50 +0100

    created a simple menu and folders

---
+++ menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene("res://main.tscn")
+
+
+func _on_istructions_pressed():
+	$istructions_text.show()
+
+
+func _on_credits_pressed():
+	$credits_text.show()
